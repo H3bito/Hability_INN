637356476cb61ecc8f83db15f4cae9775f7a9326
JUAN FELIPE TORRES TORRES <juanphenex14@gmail.com>
2023-10-15 16:03:43 -0500

    Creacion de la clase comentarios, y correcion en la clase pedido

---
+++ habilidades.shared/Entidades/Comentarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace habilidades.shared.Entidades
+{
+    public class Comentarios: Identity
+    {
+        [Display(Name = "Descripcion")]
+        [MaxLength(200, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Descripcion { get; set; }
+
+        [Display(Name = "rate")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public int rate { get; set; }
+    }
+}
